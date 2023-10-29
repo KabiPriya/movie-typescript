6b2a39bdb4cc8cab786c39c85fefb119aa677351
KabiPriya <kabipriya.t@gmail.com>
2023-10-29 23:11:12 +0530

    added components

--- app/src/components/DeleteDialog.tsx
+++ app/src/components/DeleteDialog.tsx
@@ -1,23 +1,23 @@
-// import React from "react";
+import React from "react";
 
-// interface DeleteDialogProps {
-//   isOpen: boolean;
-//   onClose: () => void;
-// }
+interface DeleteDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
 
-// const DeleteDialog: React.FC<DeleteDialogProps> = ({ isOpen, onClose }) => {
-//   return (
-//     isOpen && (
-//       <dialog open>
-//         <article>
-//           <header>
-//             <a aria-label="Close" className="close" onClick={onClose}></a>
-//           </header>
-//           <p>Successfully deleted</p>
-//         </article>
-//       </dialog>
-//     )
-//   );
-// };
+const DeleteDialog: React.FC<DeleteDialogProps> = ({ isOpen, onClose }) => {
+  return (
+    isOpen && (
+      <dialog open>
+        <article>
+          <header>
+            <a aria-label="Close" className="close" onClick={onClose}></a>
+          </header>
+          <p>Successfully deleted</p>
+        </article>
+      </dialog>
+    )
+  );
+};
 
-// export default DeleteDialog;
+export default DeleteDialog;
